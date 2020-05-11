1e0edc910573af5160f88ca2920cf94d7c84d122
Mao7122374 <1119250897@qq.com>
2020-05-11 10:45:12 +0800

    webpack-vue

--- koneko/app/main.js
+++ koneko/app/main.js
@@ -1,2 +1,21 @@
 const greeter = require('./Greeter.js');
-document.querySelector("#root").appendChild(greeter());
+document.querySelector("#app").appendChild(greeter());
+
+import Vue from "vue/dist/vue.js"
+
+var vm = new Vue({
+    el: "#app",
+    //模型层 数据
+    data: {
+        msg: "test",
+        change:"00000"
+    },
+    //所有页面 要使用的方法
+    methods: {
+        modify: function(){
+            this.msg = "after test"
+        }
+    }
+
+
+})
